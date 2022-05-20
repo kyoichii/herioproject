3d6bbfcd3348555dd9fc59253d3c357d4de07b64
Kyoichi <kyoichi.807@gmail.com>
2022-05-20 13:38:35 +0900

    [add]firebase処理 一旦保留

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+// Firebase初期化
+var config = {
+    apiKey: "AIzaSyCW8eSWJ24ifX-3YlDKnijqNLEH9BCE7Yo",
+    projectId: "herioproject",
+    storageBucket: "herioproject.appspot.com",
+    messagingSenderId: "92738578331",
+    appId: "1:92738578331:web:081c690e653c85dafa633e",
+    measurementId: "G-T1PT5W11NG"
+};
+firebase.initializeApp(config);
