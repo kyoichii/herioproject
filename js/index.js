25ea8570743acb981cea06b82c9daec43e953644
Kyoichi <kyoichi.807@gmail.com>
2022-05-20 14:33:44 +0900

    [main] ファイルの統合

--- js/index.js
+++ js/index.js
@@ -10,3 +10,25 @@ function backprocess(){
     console.log("aaaaaa")
     return false
 }
+
+let x = 100
+
+document.getElementById("back-button").onclick = function() {
+    history.back();
+     return false
+};
+
+document.getElementById("forward-button").onclick = function() {
+    history.go(1);
+    return false
+};
+
+document.getElementById("lager-button").onclick = function() {
+    x *= 1.05; document.body.style.fontSize = x + '%';
+    return false
+};
+
+document.getElementById("smaller-button").onclick = function() {
+    x /= 1.05; document.body.style.fontSize = x + '%';
+    return false
+};
