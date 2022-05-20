b719c6caf45c370cae31cf42e317e2fbddea3f64
abegin_ecccomp <2190325@ecccomp.com>
2022-05-20 13:49:32 +0900

    joy

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+let x = 100
+
 document.getElementById("back-button").onclick = function() {
     history.back();
      return false
@@ -7,4 +9,13 @@ document.getElementById("forward-button").onclick = function() {
     history.go(1);
     return false
 };
-  
+
+document.getElementById("lager-button").onclick = function() {
+    x *= 1.05; document.body.style.fontSize = x + '%';
+    return false
+};
+
+document.getElementById("smaller-button").onclick = function() {
+    x /= 1.05; document.body.style.fontSize = x + '%';
+    return false
+};
