58a960e9e94f3c28a6690e6521a6943432173951
Kyoichi <kyoichi.807@gmail.com>
2022-05-27 14:20:15 +0900

    [change] 各処理を関数に分けて識別しやすくしました。

--- js/content.js
+++ js/content.js
@@ -3,18 +3,10 @@ $(function () {
     // div要素のclass名を取得
     var className = $("div").attr("class");
     console.log(className);
-    //本体のcssを変更
-    $("." + className).wrap('<div id = "html"/>');
-    $("." + className).css('flex-grow', '2');
-    //追加したdivのcss変更  
-    $('#html').css('display', 'flex');
-    $('#html').css('flex-direction', 'row');
-    $('#html').css('height', '100%');
-    //追加したdivにさらにメイン機能の拡張機能のdiv追加
-    $("#html").prepend('<div id = "newhtml"></div>');
+    //関数cssadd呼び出し
+    cssadd(className);
     //拡張機能のhtml
-    var html = "<h2>Sylvanian Extension</h2>";
-    html += "<a href='#' target='_brank'>よく使うもの</a>"
+    htmladd();
     // aタグのクリックアクション
     $(document).ready(function () {
         $("#newhtml").find("a").click(function () {
@@ -24,9 +16,33 @@ $(function () {
         })
     })
     //拡張機能のcss
-    $('#newhtml').css('background', '#005731');
-    $('#newhtml').css('color', 'white');
-    // 拡張機能のhtml文追加
-    $("#newhtml").prepend(html);
+    excssadd();
 })
 
+//本体に拡張機能の要素を追加
+function cssadd(tagname) {
+    console.log("関数cssadd呼び出し")
+    $("." + tagname).wrap('<div id = "html"/>');
+    $("." + tagname).css('flex-grow', '2');
+    //追加したdivのcss変更  
+    $('#html').css('display', 'flex');
+    $('#html').css('flex-direction', 'row');
+    $('#html').css('height', '100%');
+    //追加したdivにさらにメイン機能の拡張機能のdiv追加
+    $("#html").prepend('<div id = "newhtml"></div>');
+}
+
+//拡張機能のhtml追加
+function htmladd(){
+    var html = "";
+    html = "<h2>Sylvanian Extension</h2>";
+    html += "<a href='#' target='_brank'>よく使うもの</a>";
+    // 拡張機能のhtml文追加
+    $("#newhtml").prepend(html);
+}
+
+//拡張機能のcss追加
+function excssadd(){
+    $('#newhtml').css('background', '#005731');
+    $('#newhtml').css('color', 'white');
+}
