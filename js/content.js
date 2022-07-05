4475e932e88b11c57d714f624c17934ed84c115b
Kyoichi <kyoichi.807@gmail.com>
2022-07-05 09:36:12 +0900

    [change]配置の変更

--- js/content.js
+++ js/content.js
@@ -5,21 +5,20 @@ $(function () {
     console.log("コンテントスクリプトだよ");
     // div要素のclass名を取得
     var className = $("div").attr("class");
-
     console.log(className);
-    //関数cssadd呼び出し
-    cssadd(className);
+    $("body").prepend('<div id = "newhtml"></div>');
+    $("#newhtml").after('<div></div>');
+    //追加したdivのcssを編集
+    cssadd()
     //拡張機能のhtml
     htmladd();
     //拡張機能の履歴html追加
     rihtml();
     //履歴htmlのcss
     ricss();
-    // aタグのクリックアクション
-
     // よく使うものクリックアクション
     $(document).ready(function () {
-        $("#newhtml").find("a").click(function () {
+        $("#newhtml").find("#usemenu").click(function () {
             //ブックマークページへ移動
             location.href = "http://127.0.0.1/html/favorite.html";
             console.log("クリック！");
@@ -32,29 +31,25 @@ $(function () {
     Cookiesave();
 
     //backボタンclickイベント
-    $('.back').on('click', function () {
+    $('.backbutton').on('click', function () {
         console.log('戻るボタンクリック')
         // // //Cokkieの値取得
-        // let backurl = getCookie('backurl')
-        // console.log('backurl:' + backurl)
         window.history.back();
     })
 
     //進むボタンclickイベント
-    $('.next').on('click', function () {
+    $('.pushbutton').on('click', function () {
         console.log('進むボタンクリック');
         // //Cokkieの値取得
-        // let moveurl = getCookie('moveurl')
-        // console.log('moveurl:' + moveurl)
         window.history.forward();
     });
 
     //プラスボタン処理
-    $('.p-button').on('click', function () {
+    $('.bigbutton').on('click', function () {
         mojichange(className, 1, mojisize);
     });
     //マイナスボタン処理
-    $('.m-button').on('click', function () {
+    $('.smallbutton').on('click', function () {
         mojichange(className, 2, mojisize);
     });
 })
@@ -83,7 +78,6 @@ function getCookie(value) {
         console.log(cArray)
         if (cArray[0] == ' ' + value) {                 // 取り出したいkeyと合致したら
             return (cArray[1])                           //value値をreturnする
-
         }
     }
 }
@@ -100,69 +94,41 @@ function mojichange(className, flg, nowsize) {
     mojisize = nowsize;
 }
 
-//本体に拡張機能の要素を追加
-function cssadd(tagname) {
-    console.log("関数cssadd呼び出し")
-    $("." + tagname).wrap('<div id = "html"/>');
-    $("." + tagname).css('flex-grow', '2');
-    //追加したdivのcss変更  
-    $('#html').css('display', 'flex');
-    $('#html').css('flex-direction', 'row');
-    $('#html').css('height', '100%');
-    //追加したdivにさらにメイン機能の拡張機能のdiv追加
-    $("#html").prepend('<div id = "newhtml"></div>');
-    $("#newhtml").prepend('<div id = "rirekihtml"></div>');
+// //本体に拡張機能の要素を追加
+function cssadd() {
+    $("#newhtml").css('position', 'fixed')
+    $("#newhtml").css('width', '100%')
+    $("#newhtml").css('z-index', '9999')
 }
 
 //拡張機能のhtml追加
 function htmladd() {
-    var html = "";
-    html = "<h1>Sylvanian Extension</h1>";
-    html += "<div id ='text'>";
-    html += "<div class='word-huge-change'>";
-    html += "<a href='#' target='_brank'>よく使うもの</a><br>";
-    html += "<div class=colorsetmain>";
-    html += "<button type=button class='back'>前のページに戻る　↲</button>";
-    html += "</div>";
-    html += "<div id ='text'>";
-    html += "<button type=button class='next'>次のページへ進む　↱</button>";
-    html += "</div>";
-    html += "</div>";
-    html += "<div class ='text'><p>文字サイズ変更</p>";
-    html += "</div>";
-    html += "<div id='button'>";
-    html += "<button type='button' class='m-button'>-</button>";
-    html += "<button type='button' class='p-button'>+</button>";
-    html += "</div>";
-    html += "</div>";
-    html += "</div>";
-    html += "</div>";
-
+    var html = "<h1>Sylvanian Extension</h1>"
+    html += "<div id = 'extensiontype'>"
+    html += "<p id = 'usemenu'>よく使うもの</p>"
+    html += "<p class = 'backbutton'>前のページへ戻る&nbsp;<span style = 'font-size:16px;'>↲</span></p>"
+    html += "<p class = 'pushbutton'>次のページへ進む&nbsp;<span style = 'font-size:16px;'>↱</span></p>"
+    html += "<div id = 'mojichange'><p>文字サイズ変更&nbsp;</p>"
+    html += "<div style = 'display:flex; justify-content: space-between;'>"
+    html += "<div class = 'smallbutton' style = 'font-size:18px;'>ー</div>"
+    html += "<div class = 'bigbutton' style = 'font-size:18px;'>＋</div>"
+    html += "</div>"
+    html += "</div>"    //id = mojichangeの終端
+    html += "<p id = 'rireki'>履歴</p>"
+    html += "</div>"    //id = extensiontypeの終端
     // 拡張機能のhtml文追加
     $("#newhtml").prepend(html);
 }
 
 //拡張機能のcss追加
 function excssadd() {
-    $('#newhtml').css('background', '#005731');
-    $('#newhtml').css('color', 'white');
-    $('h1').css('font-weight', 'bold');
+    $('#newhtml').css('background', '#005731')
+    $('#newhtml').css('color', 'white')
     $('h1').css('border-bottom', '1px solid white');
-    $('h2').css('font-weight', 'bold');
-    $('h2').css('border-bottom', '1px solid white');
-    $('.colorsetmain').css('color', 'white');
-    $('.back').css('line-height', '2ex');
-    $('#button').css('display', 'flex');
-    $('#button').css('text', '50px');
-    $('#button').css('justify-content', 'space-evenly');
-    $('#text').css('text-align', 'center');
-    $('.p-button').css('border', 'solid 2px white');
-    $('.p-button').css('height', '25px');
-    $('.p-button').css('width', '15px');
-    $('.p-button').css('margin: 50px auto 0 auto');
-    $('.m-button').css('border', 'solid 2px white');
-    $('.m-button').css('width', '15px');
-    $('.m-button').css('height', '25px');
+    $('#extensiontype').css('display','flex')
+    $('#extensiontype').css('justify-content', 'space-evenly');
+    $('#extensiontype').css('font-size', '20px');
+    $('#extensiontype').css('text-align', 'center');
 }
 
 // 拡張機能の履歴機能html追加
